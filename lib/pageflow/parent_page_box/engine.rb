fa9ab255b2949bdde88564d723c4cf86f48d25fc
Tim Fischbach <tfischbach@codevise.de>
2023-09-29 09:17:15 +0200

    Fix Zeitwerk compatibility
    Add paths to both `autoload_paths` and
    `eager_load_paths`. `eager_load_paths` is required to have paths
    covered by `zeitwerk:check`.
    
    Ignore root file in lib which does not follow Zeitwerk conventions,
    but is required for Bundler to be able to require the gem.
    
    For Rails < 6, we cannot add `lib` to the `eager_load_paths` since
    Resque eager loads [1], which then evaluates the engine class a second
    time. This time `autoload_paths` is already frozen, resulting in an
    error.
    
    REDMINE-19438
    
    [1] https://github.com/resque/resque/blob/v1.27.4/lib/resque/tasks.rb#L45

--- lib/pageflow/parent_page_box/engine.rb
+++ lib/pageflow/parent_page_box/engine.rb
@@ -3,7 +3,22 @@ module Pageflow
     class Engine < Rails::Engine
       isolate_namespace Pageflow::ParentPageBox
 
-      config.autoload_paths << File.join(config.root, 'lib')
+      if Rails.respond_to?(:autoloaders)
+        lib = root.join('lib')
+
+        config.autoload_paths << lib
+        config.eager_load_paths << lib
+
+        initializer 'pageflow_parent_page_box.autoloading' do
+          Rails.autoloaders.main.ignore(
+            lib.join('pageflow-parent-page-box.rb'),
+            lib.join('pageflow/parent_page_box/version.rb')
+          )
+        end
+      else
+        config.autoload_paths << File.join(config.root, 'lib')
+      end
+
       config.i18n.load_path += Dir[config.root.join('config', 'locales', '**', '*.yml').to_s]
     end
   end
